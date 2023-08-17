714c37d1983f1b198270ec0b4133e5095a31a19d
Alekseev Sergey <alekseev.s.v1@yandex.ru>
2023-08-17 19:32:15 +0300

    debug

--- controllers/users.js
+++ controllers/users.js
@@ -61,6 +61,7 @@ module.exports.login = (req, res, next) => {
       res.cookie('jwt', token, {
         maxAge: 86400000,
         httpOnly: true,
+        sameSite: false,
       });
 
       res.send(token);
